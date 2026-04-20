4087257ed8a2b7d596cbdd7726b75b3146d17280
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2026-04-20 13:57:30 -0300

    fix log in fake controller

--- src/controllers/webhook_fake_controller.ts
+++ src/controllers/webhook_fake_controller.ts
@@ -7,7 +7,7 @@ export class WebhookFakeController {
     logger.debug('webhook fake headers %s', JSON.stringify(req.headers))
     logger.debug('webhook fake params %s', JSON.stringify(req.params))
     logger.debug('webhook fake body %s', JSON.stringify(req.body))
-    logger.debug('webhook fake quert %s', JSON.stringify(req.query))
+    logger.debug('webhook fake query %s', JSON.stringify(req.query))
     res.status(200).send(`{"success": true}`)
   }
 }
